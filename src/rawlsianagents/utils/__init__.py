3d9d79d06575e8e557e84e4cbca1fe7d4942daf8
Sergio Ferro <Sergio_ferro@epam.com>
2026-03-14 16:50:01 -0300

    fix: isort feault settings.

--- src/rawlsianagents/utils/__init__.py
+++ src/rawlsianagents/utils/__init__.py
@@ -1,10 +1,7 @@
 """Utility helpers for metrics and analysis."""
 
-from .metrics import (
-    DEFAULT_CROSS_ENCODER_MODEL,
-    SemanticDistanceMetrics,
-    compute_claim_semantic_distance,
-)
+from .metrics import (DEFAULT_CROSS_ENCODER_MODEL, SemanticDistanceMetrics,
+                      compute_claim_semantic_distance)
 
 __all__ = [
     "DEFAULT_CROSS_ENCODER_MODEL",
